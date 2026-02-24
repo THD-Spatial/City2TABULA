4b0425ff524072d39e664762f9038b53bdf4cf71
Jaykumar Ravani <jaykumar.ravani@th-deg.de>
2026-02-24 15:02:28 +0100

    Refactor logging output to use strings.Repeat for consistency in separator formatting

--- internal/utils/print.go
+++ internal/utils/print.go
@@ -1,6 +1,7 @@
 package utils
 
 import (
+	"strings"
 	"time"
 )
 
@@ -8,7 +9,7 @@ import (
 func PrintJobInfo(jobID, jobType string, createdAt time.Time, buildingIDs []int64, tableNames, schemaNames []string) {
 	Info.Printf("") // extra spacing before block
 	Info.Printf("Job Details:")
-	Info.Printf("----------------------------------")
+	Info.Printf("%s", strings.Repeat("-", 40))
 	Info.Printf("Job ID               : %s", jobID)
 	Info.Printf("Job Type             : %s", jobType)
 	Info.Printf("Created At           : %s", createdAt.Format("2006-01-02 15:04:05"))
@@ -23,29 +24,29 @@ func PrintJobInfo(jobID, jobType string, createdAt time.Time, buildingIDs []int6
 
 	Info.Printf("Table Names           : %v", tableNames)
 	Info.Printf("Schema Names          : %v", schemaNames)
-	Info.Printf("----------------------------------")
+	Info.Printf("%s", strings.Repeat("-", 40))
 	Info.Printf("")
 }
 
 func PrintPipelineInfo(pipelineID string, buildingIDs []int64, jobCount int) {
 	Info.Printf("") // extra spacing before block
 	Info.Printf("Pipeline Details:")
-	Info.Printf("----------------------------------")
+	Info.Printf("%s", strings.Repeat("-", 40))
 	Info.Printf("Pipeline ID            : %s", pipelineID)
 	Info.Printf("Total Building IDs     : %d", len(buildingIDs))
 	Info.Printf("Total Jobs             : %d", jobCount)
-	Info.Printf("----------------------------------")
+	Info.Printf("%s", strings.Repeat("-", 40))
 	Info.Printf("")
 }
 
 func PrintPipelineQueueInfo(totalPipelines int, totalJobsInPipeline int) {
 	Info.Printf("") // extra spacing before block
 	Info.Printf("Pipeline Queue Details:")
-	Info.Printf("----------------------------------")
+	Info.Printf("%s", strings.Repeat("-", 40))
 	Info.Printf("Total Pipelines        : %d", totalPipelines)
 	Info.Printf("Total Jobs per Pipeline: %d", totalJobsInPipeline)
 	Info.Printf("Total Jobs             : %d", totalPipelines*totalJobsInPipeline)
-	Info.Printf("----------------------------------")
+	Info.Printf("%s", strings.Repeat("-", 40))
 	Info.Printf("")
 	Info.Printf("Extracting Features, this may take a while...")
 	Info.Printf("")
@@ -54,19 +55,19 @@ func PrintPipelineQueueInfo(totalPipelines int, totalJobsInPipeline int) {
 func PrintWorkerInfo(workerID int) {
 	Info.Printf("") // extra spacing before block
 	Info.Printf("Worker Details:")
-	Info.Printf("----------------------------------")
+	Info.Printf("%s", strings.Repeat("-", 40))
 	Info.Printf("Worker ID              : %d", workerID)
-	Info.Printf("----------------------------------")
+	Info.Printf("%s", strings.Repeat("-", 40))
 	Info.Printf("")
 }
 
 func PrintRunnerInfo(jobID, pipelineID string, totalJobsInPipeline int) {
 	Info.Printf("") // extra spacing before block
 	Info.Printf("Runner Details:")
-	Info.Printf("----------------------------------")
+	Info.Printf("%s", strings.Repeat("-", 40))
 	Info.Printf("Job ID:                %s", jobID)
 	Info.Printf("Pipeline ID:           %s", pipelineID)
 	Info.Printf("Total Jobs in Pipeline: %d", totalJobsInPipeline)
-	Info.Printf("----------------------------------")
+	Info.Printf("%s", strings.Repeat("-", 40))
 	Info.Printf("")
 }
