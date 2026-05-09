fe85dca0a6cb6d0164ba6605ef8dfee731b56015
Jay Ravani <jaykumar.ravani@th-deg.de>
2026-05-09 03:36:27 +0200

    chore: restore main sql scripts to original state from main branch

--- sql/scripts/main/02_dump_child_feat_geom.sql
+++ sql/scripts/main/02_dump_child_feat_geom.sql
@@ -1,7 +1,7 @@
 WITH new_buildings AS (
   -- Select only buildings that haven't been processed yet
   SELECT DISTINCT building_feature_id
-  FROM {city2tabula_schema}.{lod_schema}_child_feature_raw
+  FROM {city2tabula_schema}.{lod_schema}_child_feature
   WHERE building_feature_id IN {building_ids}
     AND building_feature_id NOT IN (
       SELECT DISTINCT building_feature_id
@@ -12,22 +12,18 @@ dumped AS (
   SELECT
     c.id AS child_row_id,
     c.building_feature_id,
-    c.building_objectid,
     c.surface_feature_id,
-    c.surface_objectid,
     c.objectclass_id,
     c.classname AS classname,
     (ST_Dump(c.geom)).geom AS geom
-  FROM {city2tabula_schema}.{lod_schema}_child_feature_raw c
+  FROM {city2tabula_schema}.{lod_schema}_child_feature c
   INNER JOIN new_buildings nb ON c.building_feature_id = nb.building_feature_id
 )
 INSERT INTO {city2tabula_schema}.{lod_schema}_child_feature_geom_dump (
     id,
     child_row_id,
     building_feature_id,
-    building_objectid,
     surface_feature_id,
-    surface_objectid,
     objectclass_id,
     classname,
     coord_dim,
@@ -38,9 +34,7 @@ SELECT
     gen_random_uuid() AS id,
     child_row_id,
     building_feature_id,
-    building_objectid,
     surface_feature_id,
-    surface_objectid,
     objectclass_id,
     classname,
     ST_CoordDim(geom) AS coord_dim,
